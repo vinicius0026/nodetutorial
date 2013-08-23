714d41c925f5d83ca1260d578bdb61ac66bafb9a
Vinicius Teixeira <vinicius0026@gmail.com>
2013-08-23 14:54:59 +0000

    In the middle of implementing POST requests handler

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(request, response) {
+	var postData = "";
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request received.");
+
+	request.setEncoding("utf8");
+
+	request.addListener("data", function(postDataChunk){
+	    postData += postDataChunk;
+	    console.log("Received POST data chunck '" +
+			postDataChunck + "'.");
+	});
+
+	request.addListener("end", function(){
+	    rout(handle,pathname,response,postData);
+	});
 
 	route(handle, pathname, response);
     }
